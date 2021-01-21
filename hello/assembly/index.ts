4ba20ab5c530853af23e0dd4b81fc4ceaeaf78f6
Tomas Tulka <tomas.tulka@gmail.com>
2021-01-21 20:08:29 +0100

    hello

--- hello/assembly/index.ts
+++ hello/assembly/index.ts
@@ -3,3 +3,10 @@ declare function print(n: i32): void;
 export function main(): void {
   print(42);
 }
+
+
+export function hello(
+    name: string): string {
+
+  return "Hello, " + name + "!";
+}
--- hello/index.js
+++ hello/index.js
@@ -12,3 +12,20 @@ var wasm = loader.instantiateSync(
   imports);
 
 wasm.exports.main();
+
+var { hello, 
+  __newString,
+  __getString, 
+  __retain,
+  __release } = wasm.exports;
+
+var pti = __retain(__newString("Tomas"));
+
+var pto = hello(pti);
+
+var str = __getString(pto);
+
+__release(pti);
+__release(pto);
+
+console.log(str);
