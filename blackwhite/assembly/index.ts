0e7d229ae6898769ac96050cd6e664f9415101b4
Tomas Tulka <tomas.tulka@gmail.com>
2021-01-27 21:13:07 +0100

    invert ready

--- blackwhite/assembly/index.ts
+++ blackwhite/assembly/index.ts
@@ -1,5 +1,27 @@
-// The entry file of your WebAssembly module.
+declare function print(n: i32): void;
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+export function invert(width: i32, height: i32): void {
+  print(width);
+  print(height);
+
+  for (let i = 0; i < width * height * 4; i += 4 /*rgba*/) {    
+    store<u8>(
+      i,
+      u8(255 - load<u8>(i))
+    );
+    store<u8>(
+      i + 1,
+      255 - load<u8>(i + 1)
+    );
+    store<u8>(
+      i + 2,
+      255 - load<u8>(i + 2)
+    );
+
+    /*
+    data[i]     = 255 - data[i];     // red
+    data[i + 1] = 255 - data[i + 1]; // green
+    data[i + 2] = 255 - data[i + 2]; // blue
+    */
+  }
 }
--- blackwhite/index.js
+++ blackwhite/index.js
@@ -1,5 +1,50 @@
-const fs = require("fs");
-const loader = require("@assemblyscript/loader");
-const imports = { /* imports go here */ };
-const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
-module.exports = wasmModule.exports;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const [width, height] = [canvas.width, canvas.height];
+
+const img = new Image();
+img.src = './waterlily.png';
+img.crossOrigin = 'anonymous';
+img.onload = () => {
+  ctx.drawImage(img, 0, 0, width, height);
+
+  const imageData = ctx.getImageData(0, 0, width, height);
+  const argb = new Uint8Array(imageData.data.buffer);
+  const data = imageData.data;
+
+  const arraySize = (width * height * 4) >>> 0;
+  const nPages = ((arraySize + 0xffff) & ~0xffff) >>> 16;
+  const memory = new WebAssembly.Memory({ initial: nPages });
+
+  WebAssembly
+    .instantiateStreaming(fetch('./build/optimized.wasm'), {
+      env:{
+        memory, // npm run asbuild:untouched -- --importMemory
+        abort: (_msg, _file, line, column) => console.error("Abort", line, column)
+      },
+      index: { print: console.log }
+    })
+    .then(({instance}) => {
+      console.log(instance.exports);
+      const {
+        invert,
+        memory
+      } = instance.exports;
+
+      let bytes = new Uint8Array(memory.buffer);
+
+      // load bytes into memory
+      bytes = new Uint8Array(memory.buffer);
+
+      for (let i = 0; i < data.length; i++)
+        bytes[i] = data[i];
+
+      invert(width, height);
+  
+      // load data from memory
+      for (let i = 0; i < bytes.length; i++) 
+         argb[i] = bytes[i];
+
+      ctx.putImageData(imageData, 0, 0);
+    });
+}
