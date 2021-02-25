09144cd84f89eaa06d83d04220945be05565f111
Tomas Tulka <tomas.tulka@gmail.com>
2021-02-25 19:18:50 +0100

    zoom view

--- mandelbrot/index.js
+++ mandelbrot/index.js
@@ -2,15 +2,31 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const [width, height] = [canvas.width, canvas.height];
 
+let offsetX = 0, offsetY = 0, ratio = 1.0;
+
 // initial plotting
 plotMandelbrot();
 
 // control elements
-document.querySelectorAll('.control').forEach(c => c.onchange = async () => {
+document.querySelector('#reset').onclick = () => redrawCanvas(plotMandelbrot);
+document.querySelectorAll('.control').forEach(c => c.onchange = () => redrawCanvas(drawCanvas));
+
+async function drawCanvas() {
+  const iterations = document.querySelector('#iterations').value;
+  const colorR = document.querySelector('#colorR').value;
+  const colorG = document.querySelector('#colorG').value;
+  const colorB = document.querySelector('#colorB').value;
+
+  await plotMandelbrot(iterations, colorR, colorG, colorB, offsetX, offsetY, ratio);
+}
+
+canvas.style.overflow = 'hidden';
+
+async function redrawCanvas(plottingFn) {
   canvas.style.filter = 'blur(3px)';
-  await plotMandelbrot();
+  await plottingFn();
   canvas.style.filter = 'initial';
-});
+}
 
 // zoom view
 const zoomView = { width: 195, height: 138 };
@@ -18,19 +34,34 @@ const zoomCanvas = document.createElement('CANVAS');
 zoomCanvas.width = width;
 zoomCanvas.height = height;
 zoomCanvas.style.position = 'absolute';
+zoomCanvas.style.zIndex = 1;
 canvas.parentElement.appendChild(zoomCanvas);
 
 const zoomViewCtx = zoomCanvas.getContext('2d');
 
-zoomViewCtx.strokeStyle = 'lime';
+ratio = zoomView.width / width;
+
+zoomViewCtx.strokeStyle = 'yellow';
 zoomCanvas.onmousemove = e => {
+  offsetX = Math.min(Math.max(Math.floor(e.offsetX - zoomView.width / 2), 0), width - zoomView.width);
+  offsetY = Math.min(Math.max(Math.floor(e.offsetY - zoomView.height / 2), 0), height - zoomView.height);
+
   zoomViewCtx.clearRect(0, 0, width, height);
-  zoomViewCtx.strokeRect(e.clientX - zoomView.width / 2, e.clientY - zoomView.height / 2, zoomView.width, zoomView.height);
+  zoomViewCtx.strokeRect(offsetX, offsetY, zoomView.width, zoomView.height);
 };
 zoomCanvas.onmouseout = () => zoomViewCtx.clearRect(0, 0, width, height);
 zoomCanvas.onclick = e => {
   
+  console.log(offsetX, offsetY, ratio);
 };
+zoomCanvas.onmousedown = e => {
+  if (e.button === 2) { // right mouse button
+    zoomCanvas.style.display = 'none';
+  }
+}
+canvas.onmousemove = e => {
+  zoomCanvas.style.display = 'initial';
+}
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 // Wasm glue functions
@@ -50,22 +81,16 @@ async function loadWasm() {
     return wasm.instance.exports;
 }
 
-async function plotMandelbrot() {
+async function plotMandelbrot(iterations, colorR, colorG, colorB, offsetX, offsetY, ratio) {
   const wasm = await loadWasm();
 
   const imageData = ctx.getImageData(0, 0, width, height);
   const bytes = new Uint8ClampedArray(wasm.memory.buffer);
 
-  const iterations = document.querySelector('#iterations').value;
-  const colorR = document.querySelector('#colorR').value;
-  const colorG = document.querySelector('#colorG').value;
-  const colorB = document.querySelector('#colorB').value;
-
-
   const timeMeasurementLabel = 'Plotting time';
   console.time(timeMeasurementLabel);
 
-  wasm.mandelbrot(width, height, iterations, colorR, colorG, colorB);
+  wasm.mandelbrot(width, height, iterations, colorR, colorG, colorB, offsetX, offsetY, ratio);
 
   console.timeEnd(timeMeasurementLabel);
 
