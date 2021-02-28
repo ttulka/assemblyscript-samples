73d6aa312eafc36ef441a40fa2aef06a60576f70
Tomas Tulka <tomas.tulka@gmail.com>
2021-02-28 09:49:35 +0100

    updated

--- memory/index.js
+++ memory/index.js
@@ -4,7 +4,7 @@ const wasm = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimize
 
 var { inc, hello, multiply, multiplyByTwo, Int32Array_ID,
   memory, 
-  __getString, 
+  __getString, __pin, __unpin, __new,
   __newString, 
   __getArray,
   __newArray } = wasm.exports;
@@ -17,16 +17,50 @@ console.log(inc(42));
 // ////////////////////////////////////////////////
 // strings
 
-var input = "Tomas";
+// convenient 
+{
+const input = "Tomas";
 
 // retain reference to string object
-var pti = __newString(input);
+const pti = __newString(input);
 
 // call wasm, pointer output
-var str = __getString(hello(pti));
+const str = __getString(hello(pti));
 
 console.log(str);
+}
 
+// direct
+{
+const input = "Tomas";
+const length = input.length;
+
+// allocate memory (usize, String (id=1))
+const pt = __new(length << 1, 1);
+
+// load bytes into memory
+const ibytes = new Uint16Array(memory.buffer);
+for (let i = 0, p = pt >>> 1; i < length; ++i) 
+  ibytes[p + i] = input.charCodeAt(i);
+
+// pin object 
+const pti = __pin(pt);
+
+// call wasm
+const pto = __pin(hello(pti));
+
+const SIZE_OFFSET = 4;
+const olength = new Uint32Array(memory.buffer)[pto - SIZE_OFFSET >>> 2];
+console.log("olength", olength)
+const obytes = new Uint8Array(memory.buffer, pto, olength);
+const str = new TextDecoder('utf8').decode(obytes);
+
+// unpin objects for GC
+__unpin(pti);
+__unpin(pto);
+
+console.log(str);
+}
 // ////////////////////////////////////////////////
 // arrays
 
