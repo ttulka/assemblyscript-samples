abd62eb2668166365a38cc3ed99273a911d177a9
Tomas Tulka <tomas.tulka@gmail.com>
2021-02-28 08:59:01 +0100

    gc correctly used

--- memory/index.js
+++ memory/index.js
@@ -5,10 +5,11 @@ const wasm = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimize
 var { inc, hello, multiply, multiplyByTwo, Int32Array_ID,
   memory, 
   __getString, 
+  __newString, 
   __getArray,
   __newArray,
   __new,
-  __pin,
+  __pin, __collect,
   __unpin } = wasm.exports;
 
 // ////////////////////////////////////////////////
@@ -20,23 +21,12 @@ console.log(inc(42));
 // strings
 
 var input = "Tomas";
-var length = input.length;
 
-// allocate memory (usize, String (id=1))
-var pt = __new(length << 1, 1);
-
-// load bytes into memory
-var bytes = new Uint16Array(memory.buffer);
-for (var i = 0, p = pt >>> 1; i < length; ++i) 
-  bytes[p + i] = input.charCodeAt(i);
-
-// retain reference to object
-var pti = __pin(pt);
+// retain reference to string object
+var pti = __newString(input);
 
 // call wasm, pointer output
 var pto = __pin(hello(pti));
-
-__unpin(pti);
 
 var str = __getString(pto);
 
@@ -51,41 +41,28 @@ console.log('Int32Array_ID', Int32Array_ID);
 
 // input array
 var arr = [1, 2, 3]
-var arri = __pin(__newArray(Int32Array_ID, arr));
+var arri = __newArray(Int32Array_ID, arr);
 
 // call, output array
-var arro1 = __getArray(multiply(arri, 2));
-var arro2 = __getArray(multiplyByTwo(arri));
-
-__unpin(arri);
-__unpin(arro1);
-__unpin(arro2);
+var arro1 = __pin(multiply(arri, 2));
+var arro2 = __pin(multiplyByTwo(arri));
 
 console.log(arr);
-console.log(arro1);
-console.log(arro2);
+console.log(__getArray(arro1));
+console.log(__getArray(arro2));
+
+__unpin(arro1);
+__unpin(arro2);
 
 // ////////////////////////////////////////////////
 // testing
 
 function sayHello(name) {
-	var length = name.length;
-
-	// allocate memory (usize, String (id=1))
-	var pt = __new(length << 1, 1);
-
-	// load bytes into memory
-	var bytes = new Uint16Array(memory.buffer);
-	for (var i = 0, p = pt >>> 1; i < length; ++i) 
-	  bytes[p + i] = name.charCodeAt(i);
-
 	// retain reference to object
-	var pti = __pin(pt);
+	var pti = __newString(name);
 
 	// call wasm, pointer output
 	var pto = __pin(hello(pti));
-
-	__unpin(pti);
 
 	var str = __getString(pto);
 
