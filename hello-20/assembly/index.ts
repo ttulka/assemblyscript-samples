a9e088824675ec7423173522736b42d917c666c3
Tomas Tulka <dev@ttulka.com>
2022-04-05 21:07:02 +0200

    strings and arrays

--- hello-20/assembly/index.ts
+++ hello-20/assembly/index.ts
@@ -4,3 +4,15 @@ declare function print(n: i32): void;
 export function main(): void {
   print(42);
 }
+
+export function hello(name: string): string {
+  return "Hello, " + name + "!";
+}
+
+export function multiply(matrix: Int32Array, x: i32): Int32Array {
+  var arr = new Int32Array(matrix.length);
+  for (var i = 0; i < matrix.length; i++) {
+    arr[i] = matrix[i] * x;
+  }
+  return arr;
+}
--- hello-20/index.js
+++ hello-20/index.js
@@ -1,2 +1,60 @@
-import { main } from "./build/release.js";
+import { main, hello, multiply } from "./build/release.js";
+
 main();
+
+var greeting = hello("Joe");
+console.log(greeting);
+
+var result = multiply([1, 2, 3], 2)
+console.log(result);
+
+
+
+var url = "./build/release.wasm";
+var module = await (
+  typeof globalThis.fetch === "function"
+    ? WebAssembly.compileStreaming(globalThis.fetch(url))
+    : WebAssembly.compile((await import("node:fs")).readFileSync(url))
+)
+var imports = {
+    env: {
+        "console.log": console.log,
+        abort: () => {}
+    }
+}
+
+const { exports: {
+    hello: hello2, memory,
+    __new, __pin, __unpin
+}} = await WebAssembly.instantiate(module, imports);
+
+var input = "Tomas";
+var length = input.length;
+
+// allocate memory (usize, string [class id=1])
+var pt = __new(length << 1, 1);
+
+// load bytes into memory (chars in AS are 16-bit)
+var ibytes = new Uint16Array(memory.buffer);
+for (let i = 0, p = pt >>> 1; i < length; ++i) 
+ibytes[p + i] = input.charCodeAt(i);
+
+// pin object 
+var pti = __pin(pt);
+
+// call wasm
+var pto = __pin(hello2(pti));
+
+// retrieve string size (uint is 32-bit)
+var SIZE_OFFSET = -4;
+var olength = new Uint32Array(memory.buffer)[pto + SIZE_OFFSET >>> 2];
+
+// load string from memory
+var obytes = new Uint8Array(memory.buffer, pto, olength);
+var str = new TextDecoder('utf8').decode(obytes);
+
+// unpin objects for GC
+__unpin(pti);
+__unpin(pto);
+
+console.log(str);
